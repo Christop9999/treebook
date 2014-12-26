89dac9cf8cf3959883e292cd789740411527da95
Chris Murolo <crmurolo@gmail.com>
2014-12-25 22:31:40 -0800

    Updated frontend for the new authenticaiton system
    added simple form
    updated several forms to bootstrap/simple form
    added login/logout register/profile links to our nav
    added a colleciton select to our status form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  
   resources :statuses
+  devise_for :users, :controllers => {:registrations => "users/registrations"}
+
+  root to: 'statuses#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
